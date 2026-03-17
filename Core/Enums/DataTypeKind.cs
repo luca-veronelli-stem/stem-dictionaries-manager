f38de841c2bdb6596ed3d5ed0767c42e085332ab
Luca Veronelli <l.veronelli@stem.it>
2026-03-17 14:33:12 +0100

    Aggiungi enum per tipo di dato con relativi tests

---
+++ Core/Enums/DataTypeKind.cs
@@ -0,0 +1,28 @@
+namespace Core.Enums;
+
+/// <summary>
+/// Tipo di dato di una variabile.
+/// </summary>
+public enum DataTypeKind
+{
+    UInt8,
+    UInt16,
+    UInt32,
+    Int8,
+    Int16,
+    Int32,
+    Float,
+    Bool,
+    
+    /// <summary>Stringa con lunghezza massima (es. String[20])</summary>
+    String,
+    
+    /// <summary>Valore bitmapped con N word (es. "due word uint16_t bitmapped")</summary>
+    Bitmapped,
+    
+    /// <summary>Array di elementi (es. "3*uint32_t")</summary>
+    Array,
+    
+    /// <summary>Tipo non riconosciuto (fallback)</summary>
+    Other
+}
