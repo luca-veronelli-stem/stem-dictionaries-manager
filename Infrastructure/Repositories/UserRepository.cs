19fd7e8795d74dab4868bd8dd50d173c4a0641a2
Luca Veronelli <l.veronelli@stem.it>
2026-03-17 16:16:13 +0100

    Aggiungi interfaccia ed implementazione per la repository degli user

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository : RepositoryBase<UserEntity>, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<UserEntity?> GetByUsernameAsync(string username, 
+        CancellationToken cancellationToken = default)
+    {
+        var normalizedUsername = username.ToLowerInvariant();
+        return await DbSet
+            .FirstOrDefaultAsync(u => u.Username == normalizedUsername, cancellationToken);
+    }
+}
