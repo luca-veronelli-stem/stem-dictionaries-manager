4f562ab6492c89e63620794a53b79b1f6834f6c9
Luca Veronelli <l.veronelli@stem.it>
2026-03-19 09:42:18 +0100

    Aggiungi Views per UserList e Settings

---
+++ GUI.Windows/Views/SettingsView.xaml.cs
@@ -0,0 +1,24 @@
+using System.Windows;
+using System.Windows.Controls;
+using GUI.Windows.ViewModels;
+
+namespace GUI.Windows.Views;
+
+/// <summary>
+/// Vista per le impostazioni dell'applicazione (stub).
+/// </summary>
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+
+    private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is SettingsViewModel viewModel)
+        {
+            await viewModel.InitializeAsync();
+        }
+    }
+}
