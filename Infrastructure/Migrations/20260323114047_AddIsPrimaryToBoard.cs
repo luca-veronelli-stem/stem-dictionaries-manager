ce9b6c6f33029060b3b4c894eea011b20c44a8d3
Luca Veronelli <l.veronelli@stem.it>
2026-03-23 12:56:06 +0100

    Aggiungi migration AddIsPrimaryToBoard

---
+++ Infrastructure/Migrations/20260323114047_AddIsPrimaryToBoard.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPrimaryToBoard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "Boards",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "Boards");
+        }
+    }
+}
