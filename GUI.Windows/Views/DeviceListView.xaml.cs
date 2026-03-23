899464aa0bd633dbc2a767d2500eefbdbf4d74cc
Luca Veronelli <l.veronelli@stem.it>
2026-03-23 10:45:39 +0100

    Aggiungi viste per lista e dettagli dispositivo

---
+++ GUI.Windows/Views/DeviceListView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace GUI.Windows.Views;
+
+public partial class DeviceListView : UserControl
+{
+    public DeviceListView()
+    {
+        InitializeComponent();
+    }
+}
