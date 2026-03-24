80536de8e59f75623ff1003f6b8bd55d35a12488
Luca Veronelli <l.veronelli@stem.it>
2026-03-24 12:34:47 +0100

    Rimuovi Entity, Repository e IRepository per BoardType

--- Infrastructure/Interfaces/IBoardTypeRepository.cs
+++
@@ -1,9 +0,0 @@
-using Infrastructure.Entities;
-
-namespace Infrastructure.Interfaces;
-
-public interface IBoardTypeRepository : IRepository<BoardTypeEntity>
-{
-    Task<BoardTypeEntity?> GetByNameAsync(string name, CancellationToken cancellationToken = default);
-    Task<BoardTypeEntity?> GetByFirmwareTypeAsync(int firmwareType, CancellationToken cancellationToken = default);
-}
